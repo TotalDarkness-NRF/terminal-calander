27730bd50206f356137494ad3daece2ea9e661b5
TotalDarkness-NRF <totaldarknesofficial@gmail.com>
2021-07-14 14:14:36 -0400

    Add simplifications to parsing colors

--- src/config.rs
+++ src/config.rs
@@ -57,36 +57,27 @@ impl Config {
             }
             let (config_var, value) = line.split_at(split_index);
             let value = value.replace(" ", "").replace("=", "");
+            let config_var = config_var.trim();
             let value = value.trim();
-            match config_var.trim() {
-                "bg_color" =>
-                    config.bg_color =
-                    parse_color(value)
-                    .unwrap_or(config.bg_color),
-                "calendar_bg_color" => 
-                    config.calendar_bg_color =
-                        parse_color(value)
-                        .unwrap_or(config.calendar_bg_color),
-                "date_bg_color" => 
-                    config.date_bg_color =
-                        parse_color(value)
-                        .unwrap_or(config.date_bg_color),
-                "date_num_color" => 
-                    config.date_num_color =
-                        parse_color(value)
-                        .unwrap_or(config.date_num_color),
-                "weekday_bg_color" => 
-                    config.weekday_bg_color =
-                        parse_color(value)
-                        .unwrap_or(config.weekday_bg_color),
-                "select_bg_color" => config.select_bg_color =
-                parse_color(value)
-                .unwrap_or(config.select_bg_color),
-                // TODO Keys
-                _ => continue,
+            if config_var.contains("color") {
+                Config::match_colors(&mut config, config_var, value);
             }
         }
         config
+    }
+
+    fn match_colors(config: &mut Config, config_var: &str, value: &str) {
+        if let Some(value) = parse_color(value) {
+            match config_var {
+                "bg_color" =>  config.bg_color = value,
+                "calendar_bg_color" => config.calendar_bg_color = value,
+                "date_bg_color" => config.date_bg_color = value,
+                "date_num_color" =>  config.date_num_color = value,
+                "weekday_bg_color" => config.weekday_bg_color = value,
+                "select_bg_color" => config.select_bg_color = value,
+                _ => (),
+            }
+        }
     }
 
     fn get_default_config() -> Self {
@@ -106,27 +97,27 @@ impl Config {
     }
 }
 
-fn parse_color(mut color_string: &str) -> Result<AnsiValue, ()> {
+fn parse_color(mut color_string: &str) -> Option<AnsiValue> {
     if color_string.starts_with("high-intensity") {
         color_string = color_string.split_at("high-intensity".len()).1;
     }
     match color_string {
-        "black" | "0" => Ok(AnsiValue(0)),
-        "red" | "1" => Ok(AnsiValue(1)),
-        "green" | "2" => Ok(AnsiValue(2)),
-        "yellow" | "3" => Ok(AnsiValue(3)),
-        "blue" | "4" => Ok(AnsiValue(4)),
-        "magenta" | "5" => Ok(AnsiValue(5)),
-        "cyan" | "6" => Ok(AnsiValue(6)),
-        "white" | "7" => Ok(AnsiValue(7)),
-        "lightblack" | "8" => Ok(AnsiValue(8)),
-        "lightred" | "9" => Ok(AnsiValue(9)),
-        "lightgreen" | "10" => Ok(AnsiValue(10)),
-        "lightyellow" | "11" => Ok(AnsiValue(11)),
-        "lightblue" | "12" => Ok(AnsiValue(12)),
-        "lightmagenta" | "13" => Ok(AnsiValue(13)),
-        "lightcyan" | "14" => Ok(AnsiValue(14)),
-        "lightwhite" | "15" => Ok(AnsiValue(15)),
-        _ => Err(()),
+        "black" | "0" => Some(AnsiValue(0)),
+        "red" | "1" => Some(AnsiValue(1)),
+        "green" | "2" => Some(AnsiValue(2)),
+        "yellow" | "3" => Some(AnsiValue(3)),
+        "blue" | "4" => Some(AnsiValue(4)),
+        "magenta" | "5" => Some(AnsiValue(5)),
+        "cyan" | "6" => Some(AnsiValue(6)),
+        "white" | "7" => Some(AnsiValue(7)),
+        "lightblack" | "8" => Some(AnsiValue(8)),
+        "lightred" | "9" => Some(AnsiValue(9)),
+        "lightgreen" | "10" => Some(AnsiValue(10)),
+        "lightyellow" | "11" => Some(AnsiValue(11)),
+        "lightblue" | "12" => Some(AnsiValue(12)),
+        "lightmagenta" | "13" => Some(AnsiValue(13)),
+        "lightcyan" | "14" => Some(AnsiValue(14)),
+        "lightwhite" | "15" => Some(AnsiValue(15)),
+        _ => None,
     }
 }
